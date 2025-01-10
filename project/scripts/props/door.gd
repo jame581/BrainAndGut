0443f759e57dec5a5e39b1f1190daa97ce0e082e
Jan Mesarč <jame_581@windowslive.com>
2025-01-10 23:18:47 +0100

    Door - action setup (open/close)

--- project/scripts/props/door.gd
+++ project/scripts/props/door.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
+enum DoorAction { OPEN, CLOSE }
 
 @export_category("Door Setup")
 @export var locked: bool = false
+@export var action: DoorAction = DoorAction.OPEN
 @export_subgroup("Interaction Setup")
 @export var cursor_texture: Texture #= preload("res://assets/sprites/cursor.png")
 @export var interaction_allowed: Global.InteractionAllowed = Global.InteractionAllowed.BOTH
@@ -12,6 +14,7 @@ extends Node2D
 @onready var interaction_area: InteractionArea = $InteractionArea
 @onready var audio_stream: AudioStreamPlayer = $AudioStreamPlayer
 
+var is_open: bool = false
 
 func _ready() -> void:
 	interaction_area.cursor_texture = cursor_texture
@@ -20,7 +23,13 @@ func _ready() -> void:
 
 func _on_interaction_area_interacted() -> void:
 	print("Interaction with door")
-	animation_player.play("open")
+	if action == DoorAction.OPEN and not is_open:
+		animation_player.play("open_door")
+		is_open = true
+	elif action == DoorAction.CLOSE and is_open:
+		animation_player.play("close_door")
+		is_open = false
+
 
 
 func unlock() -> void:
