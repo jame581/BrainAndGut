d815422943807530ca713308599d2e0c8e741b5f
Jan Mesarč <jame_581@windowslive.com>
2025-01-12 00:07:56 +0100

    Main menu - button sound added

--- project/scripts/ui/main_menu.gd
+++ project/scripts/ui/main_menu.gd
@@ -1,10 +1,13 @@
 extends Control
 
+@export_category("Main Menu Data")
+@export var ui_click_sound: AudioStream = preload("res://assets/sounds/buttons/ESM_Perfect_App_Button_5_Organic_Simple_Classic_Game_Click.wav")
 
 @onready var exit_button: Button = $MarginContainer/HBoxContainer/RightContainer/ButtonsContainer/ExitButton
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var credit_panel: Panel = $MarginContainer/HBoxContainer/LeftContainer/MarginContainer/CreditsPanel
 @onready var version_label: Label = $MarginContainer/HBoxContainer/RightContainer/VBoxContainer2/VersionLabel
+@onready var audio_player: AudioStreamPlayer = $UIAudioStreamPlayer
 
 var animation_in_progress: bool = false
 
@@ -15,11 +18,13 @@ func _ready() -> void:
 	else:
 		exit_button.show()
 	
+	audio_player.stream = ui_click_sound
 	credit_panel.visible = false
 	version_label.text = Global.get_game_version()
 
 
 func _on_start_button_pressed() -> void:
+	audio_player.play()
 	Global.goto_scene("res://maps/main_level.tscn")
 
 
@@ -27,6 +32,7 @@ func _on_credits_button_pressed() -> void:
 	if animation_in_progress:
 		return
 
+	audio_player.play()
 	if credit_panel.visible:
 		animation_player.play("hide_credits")
 	else:
@@ -36,6 +42,7 @@ func _on_credits_button_pressed() -> void:
 
 
 func _on_exit_button_pressed() -> void:
+	audio_player.play()
 	get_tree().quit()
 
 
