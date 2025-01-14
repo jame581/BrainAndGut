46fafa3a993385f73ba52f99ea5467e31dba787c
Jan Mesarč <jame_581@windowslive.com>
2025-01-14 20:09:42 +0100

    Main menu music and level music added

--- project/scripts/autoloads/audio_manager.gd
+++ project/scripts/autoloads/audio_manager.gd
@@ -1,15 +1,30 @@
 extends Node
 
-@onready var wave_audio_stream: AudioStreamPlayer = $WavesStream
+
+@onready var waves_stream: AudioStreamPlayer = $WavesStream
+@onready var audio_stream: AudioStreamPlayer = $AudioStream
+
+var main_menu_music: AudioStream = preload("res://assets/sounds/music/ship-menu.mp3")
+var main_level_music: AudioStream = preload("res://assets/sounds/music/shiplevel.mp3")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Global.map_changed.connect(handle_map_changed)
+	audio_stream.stream = main_menu_music
+	audio_stream.play()
 
 
 func handle_map_changed(new_map_path: String) -> void:
 	print("Audio Manager: Map changed to: ", new_map_path)
 	if new_map_path == "res://maps/main_level.tscn":
-		wave_audio_stream.play()
+		audio_stream.stream = main_level_music
+		audio_stream.play()
+		waves_stream.play()
+	elif new_map_path == "res://maps/main_menu.tscn":
+		audio_stream.stream = main_menu_music
+		audio_stream.play()
 	else:
-		wave_audio_stream.stop()
+		audio_stream.stop()
+		waves_stream.play()
+
+	
